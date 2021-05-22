f9e41d7b2315cc1f2bde1f489fad50f86e6e96df
Calin Puscas <calinpuscas@gmail.com>
2021-05-22 17:58:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  //navbar toggling
+  $("#navbar-toggler").click(function () {
+    $(".navbar-collapse").toggleClass("showNav");
+  });
+  //animated bar icon
+  $("#navbar-toggler").click(function () {
+    $(this).toggleClass("animatedNav");
+  });
+  //navbar background change on scroll
+  $(window).scroll(function () {
+    let position = $(window).scrollTop();
+    if (position >= 60) {
+      $(".navbar-wrapper").removeClass("container");
+      $(".navbar-wrapper").addClass("fxd-navbar-wrapper");
+    } else {
+      $(".navbar-wrapper").addClass("container");
+      $(".navbar-wrapper").removeClass("fxd-navbar-wrapper");
+    }
+  });
+});
